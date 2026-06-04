803905362879829fc63f0e18b940a30d7bde3ab9
Antony Rafael <depaulacostaantony@gmail.com>
2026-06-04 16:10:58 +0000

    feat: implement delete session in `api/v1/sessions`

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -6,6 +6,7 @@ import session from "model/session";
 const router = createRouter();
 
 router.post(postHandler);
+router.delete(deleteHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -23,4 +24,14 @@ async function postHandler(req, res) {
   await controller.setSessionCookie(newSession.token, res);
 
   return res.status(201).json(newSession);
+}
+
+async function deleteHandler(req, res) {
+  const sessionToken = req.cookies.session_id;
+
+  const sessionObject = await session.findOneValidByToken(sessionToken);
+  const deletedSession = await session.expireById(sessionObject.id);
+  controller.clearSessionCookie(res);
+
+  return res.status(200).json(deletedSession);
 }
