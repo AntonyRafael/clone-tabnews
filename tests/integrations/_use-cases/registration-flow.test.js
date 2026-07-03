de230b9b321d977e3068247dfae6e54e3c6e13e5
Antony Rafael <depaulacostaantony@gmail.com>
2026-07-03 14:39:48 +0000

    feat: create `injectAnonymousOrUser` and check features in `canRequest` func

--- tests/integrations/_use-cases/registration-flow.test.js
+++ tests/integrations/_use-cases/registration-flow.test.js
@@ -85,7 +85,32 @@ describe("Use case: Registration Flow (all successful)", () => {
     expect(activatedUser.features).toEqual(["create:session"]);
   });
 
-  test("Login", async () => {});
+  test("Login", async () => {
+    const createSessionResponse = await fetch(
+      "http://localhost:3000/api/v1/sessions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(
+          {
+            email: "registration.flow@email.com",
+            password: "RegistrationFlowPass",
+          },
+          null,
+          2,
+        ),
+      },
+    );
+
+    expect(createSessionResponse.status).toBe(201);
+
+    const createSessionResponseBody = await createSessionResponse.json();
+    expect(createSessionResponseBody.user_id).toBe(
+      createSessionResponseBody.user_id,
+    );
+  });
 
   test("Get user information", async () => {});
 });
